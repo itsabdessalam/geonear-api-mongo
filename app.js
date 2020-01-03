fa25c7a8dc9055484f58b47fd20a0a9a2130309f
Abdessalam BENHARIRA <belmehelabdessalam.benharira@ynov.com>
2020-01-03 04:37:12 +0100

    build: update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const dotenv = require("dotenv").config();
-const bodyParser = require("body-parser");
 const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
@@ -12,16 +11,13 @@ const app = express();
 // CORS
 app.use(cors());
 
+// Log
 app.use(logger("dev"));
+
 // security
 app.use(helmet());
+
 // body-parser
-app.use(bodyParser.json());
-app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
-);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -37,12 +33,15 @@ mongoose.connect(
 		console.log("🚀 Successfully connected to database !");
 	}
 );
+
 // routes
 app.use(require("./routes"));
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
 	next(createError(404, `Cannot ${req.method} ${req.originalUrl}`));
 });
+
 // error handler
 app.use((err, req, res, next) => {
 	res.status(err.status || 500);
